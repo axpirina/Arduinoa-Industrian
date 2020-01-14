30f5761f12d1e8988506b26f126b821f2bc035ab
axpirina <axpirina@oteitzalp.com>
2020-01-14 12:40:06 +0100

    Update LDR_LED.ino

--- Auto baten argi Automatikoak (MEKA)/LDR_LED.ino
+++ Auto baten argi Automatikoak (MEKA)/LDR_LED.ino
@@ -1,18 +1,23 @@
 /*
-Arduino batekin ingurunean dugun argi kantitatea neurtu eta LED bat proportzioan pizten duen programa.
-* By TheCircuit
-* Modified by Axpi 2019
+Praktika honetan LDR (Light dependant resistor) baten bitartez argiaren aldaketa 
+tentsio elektriko aldakor batean bihurtu eta Arduino baten bitartez neurtuko dugu. 
+Ondoren argi asko baldin badago 2 lanpara LED itzaliko ditugu eta argi gutxi baldin 
+badago berriz piztu beti ere proportzioak mantenduaz.
+
+*by Axpi 2020
 * Creatives Commons
 */
  
-int argiValue = 0; // Aldagaien deklarazioa
-int led = 11; 
-  
+int argiValue = 0; // Aldagaien deklarazioa 
+int E1 = 10; // 1 Argiaren intentsitatea. PWM 0-100
+int E2 = 11; // 1 Argiaren intentsitatea. PWM 0-100
+int M1 = 12; // 1 Argiaren polaritatea. HIGH Forward LOW Reverse
+int M2 = 13; // 2 Argiaren polaritatea. HIGH Forward LOW Reverse
+
 void setup() {
  
 // Serie komunikazioa martxan jarri
 Serial.begin(9600);
-pinMode(led,OUTPUT);
 }
  
 void loop() {
@@ -23,9 +28,26 @@ argiValue = analogRead(A0);           // 0 pin analogikoa irakurri eta aldagaiea
 Serial.print("Gelako argiaren balioa 1-255 artean: ");    // Irakurririko balioa inprimatu
 Serial.println(argiValue); 
 
-int ledValue = argiValue;
-ledValue = map (ledValue, 0,255,255,0); 
-analogWrite(led,ledValue);
-
-delay(150);
+if (argiValue>200  && argiValue<250){
+     Serial.println("ARGIAK ITZALITA"); Serial.println(""); 
+     digitalWrite(M1,HIGH);
+     analogWrite(E1, 0);
+     digitalWrite(M2,HIGH);
+     analogWrite(E2, 0);
+     }
+ else if (argiValue>150  && argiValue<200){
+     Serial.println("ARGI MOTZAK"); Serial.println(""); 
+     digitalWrite(M1,HIGH);
+     analogWrite(E1, 20);
+     digitalWrite(M2,HIGH);
+     analogWrite(E2, 20);
+     }
+ else if (argiValue>0  && argiValue<150){
+     Serial.println("ARGI LUZEAK"); Serial.println(""); 
+     digitalWrite(M1,HIGH);
+     analogWrite(E1, 0);
+     digitalWrite(M2,HIGH);
+     analogWrite(E2, 0);
+     }
+delay(0);
 }
